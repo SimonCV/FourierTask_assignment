b5383b0ca87049e8ad81a524c2a0e452cdb12f9c
Therkelsen <thomastherkelsen191200@gmail.com>
2021-12-06 14:49:24 -0800

    Testing

--- openCVcourse/openCVcourse/FourierTask_assignment.cpp
+++ openCVcourse/openCVcourse/FourierTask_assignment.cpp
@@ -14,10 +14,33 @@ int maxThreshold = 100;
 // Kernel_size p  Sobel operatoren. Den algoritme der finde gradienten for samtlige pixels.
 int kernel_size = 3;
 
+string type2str(int type) {
+  string r;
+
+  uchar depth = type & CV_MAT_DEPTH_MASK;
+  uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+  switch ( depth ) {
+    case CV_8U:  r = "8U"; break;
+    case CV_8S:  r = "8S"; break;
+    case CV_16U: r = "16U"; break;
+    case CV_16S: r = "16S"; break;
+    case CV_32S: r = "32S"; break;
+    case CV_32F: r = "32F"; break;
+    case CV_64F: r = "64F"; break;
+    default:     r = "User"; break;
+  }
+
+  r += "C";
+  r += (chans+'0');
+
+  return r;
+}
+
 void cannyThreshold(Mat &src, Mat &dst) {
     // Fjerner stoej med GaussianBlur, da canny edge detection er overfoelsom overfor stoej. Der bruges en 5x5 kerne, og
-	// src overskrives, da vi ikke skal bruge det gamle billede. Kernel standard deviation i x-aksens retning p  kernen
-    // saettes til nul. P  den m de udregner OpenCV selv standard deviation. GaussianBlur bruges, da det er standard for
+    // src overskrives, da vi ikke skal bruge det gamle billede. Kernel standard deviation i x-aksens retning paa kernen
+    // saettes til nul. Paa den maade udregner OpenCV selv standard deviation. GaussianBlur bruges, da det er standard for
 	// preprocessering af canny edge detection.
     GaussianBlur(src, src, Size(5, 5), 0);
 
@@ -26,17 +49,18 @@ void cannyThreshold(Mat &src, Mat &dst) {
     Canny(src, dst, lowThreshold, maxThreshold, kernel_size, true);
 }
 
-Mat houghCircleTransform(Mat &src) {
+void houghCircleTransform(Mat &src) {
 	// Initialize the accumulator to 0
-    Mat accumulator = Mat::zeros(src.rows, src.cols, CV_8UC1);
-    Mat acc(accumulator.rows, accumulator.cols, accumulator.type());
+    Mat accumulator = Mat::zeros(Size(src.cols, src.rows), CV_8UC1);
 
 	double r = 32;
     double b = 0;
     double a = 0;
 
-    cout << "Accumulator size = " << accumulator.rows << ", " << accumulator.cols << endl;
+    cout << "Accumulator type: " << type2str(accumulator.type()) << endl;
+
     cout << "Image size       = " << src.rows << ", " << src.cols << endl;
+    cout << "Accumulator size = " << accumulator.rows << ", " << accumulator.cols << endl;
 
 	// Best candidate. (x,y) in image space to (a,b) in Hough space
     for (int x = 0; x < src.rows; x++) {
@@ -53,7 +77,41 @@ Mat houghCircleTransform(Mat &src) {
 			}
 		}
     }
-    return accumulator;
+}
+
+void newHoughCircleTransform(Mat &canny) {
+    // Initialize the accumulator to 0
+    Mat acc = Mat::zeros(Size(canny.cols, canny.rows), CV_8UC1);
+
+    double r = 32;
+    double b = 0;
+    double a = 0;
+
+    cout << "Accumulator type: " << type2str(acc.type()) << endl;
+
+    cout << "Image size       = " << canny.rows << ", " << canny.cols << endl;
+    cout << "Accumulator size = " << acc.rows << ", " << acc.cols << endl;
+
+    // Best candidate. (x,y) in image space to (a,b) in Hough space
+    for (int x = 0; x < canny.rows; x++) {
+        for (int y = 0; y < canny.cols; y++) {
+            //cout << "canny.at(" << y << ", " << x << ") = " << (int)(canny.at<uchar>(y, x)) << endl;
+            if (canny.at<uchar>(x, y) > 0) {
+                for (double theta = 0; theta < 360; theta++) {
+                    b = y - r * sin(theta * M_PI / 180);
+                    a = x - r * cos(theta * M_PI / 180);
+                    //cout << "Accum (a, b)(" << a << ", " << b << ") at (x, y, theta)(" << x << ", " << y << ", " << theta << ") = ";
+                    //if (accumulator.at<uchar>(a, b) < 255) {
+                        acc.at<uchar>(a, b) += 1;
+                    //}
+                    //cout << (int)accumulator.at<uchar>(a, b) << endl;
+                }
+            }
+        }
+        imshow("Acc", acc);
+        waitKey(1);
+    }
+
 }
 
 int main() {
@@ -64,23 +122,22 @@ int main() {
     Mat src = imread(path), dst;
     Mat hough = Mat::zeros(src.rows, src.cols, CV_8UC1);
 
+    cout << "Source type : " << type2str(src.type()) << endl;
+    cout << "Canny type  : " << type2str(dst.type()) << endl;
+    cout << "Hough type  : " << type2str(hough.type()) << endl;
+
 	if (!src.data) {
 		cerr << "No image data" << endl;
     }
 
     imshow("Original", src);
     cannyThreshold(src, dst);
+    src.release();
     imshow("Canny Edge Detection", dst);
     waitKey(0);
     destroyAllWindows();
-    //cout << "src type = " << src.type() << endl;
-    //cout << "dst type = " << dst.type() << endl;
-    cout << "test1" << endl;
-    houghCircleTransform(dst).copyTo(hough);
-    cout << "test2"  << endl;
-    //cout << "hough size out: " << hough.size << endl;
-    //imshow("Hough Circle Transform", hough);
-    //waitKey(0);
-    //destroyAllWindows();
+    //cout << "test1" << endl;
+    newHoughCircleTransform(dst);
+    //cout << "test2"  << endl;
     return 0;
 }
