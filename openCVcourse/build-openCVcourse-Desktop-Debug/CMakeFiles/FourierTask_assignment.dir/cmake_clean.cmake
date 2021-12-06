f807462ea2838897ff4bc743eb4c072bab121dbe
Therkelsen <thomastherkelsen191200@gmail.com>
2021-12-06 08:10:51 -0800

    Remove Latin characters

---
+++ openCVcourse/build-openCVcourse-Desktop-Debug/CMakeFiles/FourierTask_assignment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FourierTask_assignment.dir/FourierTask_assignment.cpp.o"
+  "FourierTask_assignment"
+  "FourierTask_assignment.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FourierTask_assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- openCVcourse/openCVcourse/FourierTask_assignment.cpp
+++ openCVcourse/openCVcourse/FourierTask_assignment.cpp
@@ -1,4 +1,4 @@
-#include <opencv2/imgcodecs.hpp>
+ï»¿#include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
@@ -10,24 +10,24 @@ using namespace std;
 int lowThreshold = 0;
 int maxThreshold = 100;
 
-// Kernel_size på Sobel operatoren. Den algoritme der finde gradienten for samtlige pixels.
+// Kernel_size p  Sobel operatoren. Den algoritme der finde gradienten for samtlige pixels.
 int kernel_size = 3;
 
 static void cannyThreshold(Mat &src, Mat &dst) {
-	// Fjerner støj med GaussianBlur, da canny edge detection er overfølsom overfor støj. Der bruges en 5x5 kerne, og 
-	// src overskrives, da vi ikke skal bruge det gamle billede. Kernel standard deviation i x-aksens retning på kernen
-	// sættes til nul. På den måde udregner OpenCV selv standard deviation. GaussianBlur bruges, da det er standard for
+    // Fjerner stoej med GaussianBlur, da canny edge detection er overfoelsom overfor stoej. Der bruges en 5x5 kerne, og
+	// src overskrives, da vi ikke skal bruge det gamle billede. Kernel standard deviation i x-aksens retning p  kernen
+    // saettes til nul. P  den m de udregner OpenCV selv standard deviation. GaussianBlur bruges, da det er standard for
 	// preprocessering af canny edge detection.
 	GaussianBlur(src, src, Size(5, 5), 0);
 
 	// src overskrives igen, da det gamle billede ikke skal bruges. L2gradient er sat til "true", da funktionen i givet fald
-	// bliver mere præcis med algoritment: L2 norm = sqr((dI/dx)^2+(dI/dy)^2)
+    // bliver mere praecis med algoritment: L2 norm = sqr((dI/dx)^2+(dI/dy)^2)
 	Canny(src, dst, lowThreshold, maxThreshold, kernel_size, true);
 }
 
 
 int main() {
-	string path = "imgs/billede_noisefree.PNG";
+    string path = "../openCVcourse/imgs/billede_noisefree.png";
 
 	// Normalt ville man skulle konvertere billedet til grayscale, men vores billede er allerede i sort/hvid. dst er destinationsbilledet
 	// til canny edge detect funktionen.
@@ -35,11 +35,11 @@ int main() {
 
 	if (!src.data) {
 		cerr << "No image data" << endl;
-		}
+    }
 
-	imshow("Original", src);
+    /*imshow("Original", src);
 	cannyThreshold(src, dst);
 	imshow("Canny Edge Detection", dst);
-	waitKey(0);
+    waitKey(0);*/
 	return 0;
 }
